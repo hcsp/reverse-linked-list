fff3aefdcbc2102594a69fa15ac515635338edd4
kungua <52815012+kungua@users.noreply.github.com>
2019-09-24 17:55:27 +0800

    finish reverse (#1)
    finish reverse (#1)

--- src/main/java/com/github/hcsp/datastructure/ReverseLinkedList.java
+++ src/main/java/com/github/hcsp/datastructure/ReverseLinkedList.java
@@ -18,7 +18,15 @@ public class ReverseLinkedList {
     // 传递的参数是原始链表的头节点
     // 返回翻转后的链表的头节点
     public static Node reverse(Node head) {
-        return null;
+        Node preNode = null;
+        Node currentNode = head;
+        while (currentNode != null) {
+            Node nextNode = currentNode.next;
+            currentNode.next = preNode;
+            preNode = currentNode;
+            currentNode = nextNode;
+        }
+        return preNode;
     }
 
     public static class Node {
