5cd4e9e98131c739d5d6dba0967870c57afc82ac
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2019-12-01 10:16:44 -0800

    implements the reverse linked list function

--- src/main/java/com/github/hcsp/datastructure/ReverseLinkedList.java
+++ src/main/java/com/github/hcsp/datastructure/ReverseLinkedList.java
@@ -18,7 +18,6 @@ public class ReverseLinkedList {
     // 传递的参数是原始链表的头节点
     // 返回翻转后的链表的头节点
     public static Node reverse(Node head) {
-        if (head == null || head.next == null) return head;
         Node res = null;
         while (head != null) {
             Node temp = head;
