0b277e9ffca7d6534d54a48ff4b5610d943e4544
luckzhu <luckzhu1992@gmail.com>
2020-09-30 09:34:38 +0800

    reverse-linked-list

--- src/main/java/com/github/hcsp/datastructure/ReverseLinkedList.java
+++ src/main/java/com/github/hcsp/datastructure/ReverseLinkedList.java
@@ -18,7 +18,15 @@ public class ReverseLinkedList {
     // 传递的参数是原始链表的头节点
     // 返回翻转后的链表的头节点
     public static Node reverse(Node head) {
-        return null;
+        Node pre = null;
+        Node cur = head;
+        while (cur != null) {
+            Node next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
     }
 
     public static class Node {
