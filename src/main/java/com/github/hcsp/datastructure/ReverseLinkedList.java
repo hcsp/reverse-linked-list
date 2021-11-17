d362482794db42b939dbea7251fdc9141abee255
小虾米 <freebufer.wang@gmail.com>
2021-11-17 23:00:37 +0800

    ReverseLinkedList test (#113)
    ReverseLinkedList test (#113)

--- src/main/java/com/github/hcsp/datastructure/ReverseLinkedList.java
+++ src/main/java/com/github/hcsp/datastructure/ReverseLinkedList.java
@@ -18,7 +18,13 @@ public class ReverseLinkedList {
     // 传递的参数是原始链表的头节点
     // 返回翻转后的链表的头节点
     public static Node reverse(Node head) {
-        return null;
+        if (head != null && head.next != null) {
+            Node newHead = reverse(head.next); // 一通操作,就进入到了链表的倒数第二个元素
+            head.next.next = head; // 把它的下一个元素设置成头节点
+            head.next = null; // 再把头节点的下一个元素设置为空
+            return newHead;
+        }
+        return head;
     }
 
     public static class Node {
